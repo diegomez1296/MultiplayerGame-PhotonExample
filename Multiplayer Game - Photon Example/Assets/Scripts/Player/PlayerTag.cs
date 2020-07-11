54aafdd0ddb360b78d5485481ece4bbc60ef4085
Łukasz Rydziński <rlukasz1996@gmail.com>
2020-07-11 17:54:14 +0200

    || Player Nicknames

---
+++ Multiplayer Game - Photon Example/Assets/Scripts/Player/PlayerTag.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerTag : MonoBehaviourPun
+{
+    [SerializeField] private TextMeshProUGUI nickText;
+    private Transform mainCameraTransform;
+
+    private void Start()
+    {
+        mainCameraTransform = Camera.main.transform;
+
+        if (photonView.IsMine) return;
+
+        SetNickText();
+    }
+
+    private void LateUpdate()
+    {
+        nickText.transform.LookAt(nickText.transform.position + mainCameraTransform.rotation * Vector3.forward, mainCameraTransform.rotation * Vector3.up);
+    }
+
+    private void SetNickText()
+    {
+        nickText.text = photonView.Owner.NickName;
+    }
+}
--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Simple game with multiplayer created with photon.
 
 ### Galery:
 <a href="https://imgur.com/rCXDrgS"><img src="https://i.imgur.com/rCXDrgS.png" title="source: imgur.com" /></a>
+<a href="https://imgur.com/swDKaX6"><img src="https://i.imgur.com/swDKaX6.png" title="source: imgur.com" /></a>
 <a href="https://imgur.com/QcyEjHz"><img src="https://i.imgur.com/QcyEjHz.png" title="source: imgur.com" /></a>
